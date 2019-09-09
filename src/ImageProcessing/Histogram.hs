8e9ff8e5052a6b77a49fb8195eaa8a563f5f17e6
SamuelCoral <samisito56@hotmail.com>
2019-09-09 02:04:51 -0500

    Added contrastLevel

--- src/ImageProcessing/Histogram.hs
+++ src/ImageProcessing/Histogram.hs
@@ -5,6 +5,10 @@ import Control.Lens
 import GHC.Word
 import ImageProcessing.Types
 import ImageProcessing.Geometry
+import Data.Tuple
+
+
+type Histogram = Map.Map Word8 Integer
 
 
 count :: Ord k => [k] -> Map.Map k Integer
@@ -19,18 +23,18 @@ accumSum = accumSumAux 0
             accumSumAux _ _ = []
             
 
-histogram :: E Color -> Bitmap -> Map.Map Word8 Integer
+histogram :: E Color -> Bitmap -> Histogram
 histogram method = count . concat . over pixels (view red . method)
 
 
-accumHistogram :: E Color -> Bitmap -> Map.Map Word8 Integer
+accumHistogram :: E Color -> Bitmap -> Histogram
 accumHistogram method bitmap =
     let hist = histogram method bitmap
         (k, v) = accumSum <$> unzip (Map.toList hist)
     in Map.fromList $ zip k v
 
 
-plotHistogram :: Color -> Map.Map Word8 Integer -> E Bitmap
+plotHistogram :: Color -> Histogram -> E Bitmap
 plotHistogram color hist out = 
     let (w, h) = bitmapDimensions out
         scaleX = fromIntegral w / 0x100
@@ -40,4 +44,16 @@ plotHistogram color hist out =
                 round $ fromIntegral i * scaleX,
                 h - round (fromIntegral n * scaleY)
             ) bar) out hist
+
+
+illuminationLevel :: Histogram -> Float
+illuminationLevel hist =
+    fromIntegral (sum $ uncurry (*) . fmap fromIntegral . swap <$> Map.toList hist) /
+    fromIntegral (sum hist)
+
+
+contrastLevel :: Histogram -> Word8
+contrastLevel hist =
+    let h = [ x | x <- Map.keys hist, x /= 0 ]
+    in  maximum h - minimum h
 
